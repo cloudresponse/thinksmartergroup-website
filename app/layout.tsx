e31e3799a996fd83844ede93b3e089a0978ca37e
Grant Johnson <grant.johnson@cloudresponse.co.uk>
2025-03-12 15:45:12 +0000

    Added Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "@/components/feature/NavBar";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <NavBar />
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
